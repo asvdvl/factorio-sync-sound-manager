5a9940148408d5cec03a1829ac7d3923c287abab
asvdev <57688070+asvdvl@users.noreply.github.com>
2023-12-26 01:51:24 +0300

    look at the previous commit for an explanation

--- settings.lua
+++ settings.lua
@@ -1,17 +1,15 @@
-local settings = {
+data:extend({
     {
         type = "double-setting",
         name = "volume",
         setting_type = "startup",
-        default_value = 1,
-        was_changed_in = 1
+        default_value = 1
     },
     {
         type = "bool-setting",
         name = "use_doppler_shift",
         setting_type = "startup",
-        default_value = true,
-        was_changed_in = 1
+        default_value = true
     },
     {
         type = "int-setting",
@@ -19,32 +17,18 @@ local settings = {
         setting_type = "startup",
         default_value = 1,
         minimum_value = 0,
-        maximum_value = 255,
-        was_changed_in = 0
+        maximum_value = 255
     },
     {
         type = "bool-setting",
         name = "use_simple_sound_system",
         setting_type = "startup",
-        default_value = false,
-        was_changed_in = 0
+        default_value = false
     },
     {
         type = "bool-setting",
         name = "debug",
         setting_type = "startup",
         default_value = false
-    },
-    {
-        type = "int-setting",
-        --hidden = true,
-        name = "settings-version",
-        setting_type = "startup",
-        minimum_value = 0,
-        default_value = 0   --kludge for detect old settings, aka "migrations", actualy def value = 1(will change in settings-final-fixes.lua)
-    },
-}
-if not _G.settings then
-    data:extend(settings)
-end
-return settings
+    }
+})
