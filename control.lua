5badaca77495b2bf9ebb7c4c1a8732b43dc6aeab
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-01-05 05:16:44 +0300

    mostly done updating by machine state

--- control.lua
+++ control.lua
@@ -1,1 +1,3 @@
-require("scripts.sound-emitter-manager")
+if not settings.startup["rwse-use_simple_sound_system"].value then
+    require("scripts.sound-emitter-manager")
+end
--- scripts/sound-emitter-manager.lua
+++ scripts/sound-emitter-manager.lua
@@ -1,6 +1,22 @@
 local startup = settings.startup
+local emitter_type = "simple-entity"
 local proto_type = startup["rwse-working_proto_type_custom"].value
 local proto_prefix = startup["rwse-working_proto_prefix"].value
+local validStatuses = {
+    defines.entity_status.working,
+    defines.entity_status.normal,
+    defines.entity_status.low_power,
+    ----these lines below are for future use, not now
+    --defines.entity_status.networks_connected,
+    --defines.entity_status.charging,
+    --defines.entity_status.discharging,
+    --defines.entity_status.fully_charged,
+    --defines.entity_status.low_input_fluid,
+    --defines.entity_status.preparing_rocket_for_launch,
+    --defines.entity_status.waiting_to_launch_rocket,
+    --defines.entity_status.launching_rocket,
+    --defines.entity_status.recharging_after_power_outage,
+}
 if proto_prefix == "<custom>" then
     proto_prefix = startup["rwse-working_proto_prefix_custom"].value
 end
@@ -10,15 +26,15 @@ local parentName = 'sound-emitter'
 local function debugMsg(text)
     if startup["rwse-debug"].value then
         game.print(text)
-        log(text)
+        log(text..'\n')
     else
         --I hope this is only temporary here, because in my opinion, clogging up the logs just isn’t a good idea.
-        log(text)
+        log(text..'\n')
     end
 end
 
 local function coordinateFormat(entity)
-    return '['..tostring(entity.position.x)..', '..tostring(entity.position.y)..']'
+    return tostring(entity.surface.name)..':['..tostring(entity.position.x)..', '..tostring(entity.position.y)..']'
 end
 
 local function itsRightEntity(name, dontCheck)
@@ -37,15 +53,19 @@ local function itsRightEntity(name, dontCheck)
 end
 
 local function on_entity_create(event, dontCheck)
+    local entity = event.created_entity
+    local surface = entity.surface
     --just to be sure
-    if not event.created_entity.valid then
+    if not entity.valid then
         return
     end
-
-    if itsRightEntity(event.created_entity.name, dontCheck) then
-        local entity = event.created_entity
-        local surface = entity.surface
-        debugMsg('detect new entity '..coordinateFormat(entity)..', on surface '..tostring(surface.name))
+    --also check that we don’t have an emitter yet
+    local emmiters = surface.find_entities_filtered{type = emitter_type, name = parentName..'__'..entity.name, position = entity.position}
+    if #emmiters > 0 then
+        return
+    end
+    if itsRightEntity(entity.name, dontCheck) then
+        debugMsg('detect new entity '..coordinateFormat(entity))
         surface.create_entity{name = "sound-emitter"..'__'..entity.name, position = entity.position}
     end
 end
@@ -58,7 +78,7 @@ local function on_entity_delete(event)
     if itsRightEntity(event.entity.name) then
         local entity = event.entity
         local surface = entity.surface
-        local emmiters = surface.find_entities_filtered{type = "simple-entity", name = parentName..'__'..entity.name, position = entity.position}
+        local emmiters = surface.find_entities_filtered{type = emitter_type, name = parentName..'__'..entity.name, position = entity.position}
         for _, entity in pairs(emmiters) do
             debugMsg('destroy '..coordinateFormat(entity))
             entity.destroy()
@@ -68,6 +88,10 @@ end
 
 local function on_init()
     global.used_prototypes = {}
+    global.disabled_entities_positions = {}
+    for surfaceName, _ in pairs(game.surfaces) do
+        global.disabled_entities_positions[surfaceName] = {}
+    end
     local protoPrefix = "^" .. parentName:gsub("-", "%%-")..'__'
     local i = 1
     for protoName, value in pairs(game.entity_prototypes) do
@@ -82,9 +106,9 @@ local function on_init()
     for _, surface in pairs(game.surfaces) do
         --destroy all emitters
         for _, protoName in pairs(global.used_prototypes) do
-            local emmiters = surface.find_entities_filtered{type = "simple-entity", name = parentName..'__'..protoName}
+            local emmiters = surface.find_entities_filtered{type = emitter_type, name = parentName..'__'..protoName}
             if #emmiters > 0 then
-                debugMsg('found '..tostring(#emmiters)..' emitters, on surface '..tostring(surface.name))
+                debugMsg('found '..tostring(#emmiters)..' emitters')
                 for _, entity in pairs(emmiters) do
                     debugMsg('destroy '..coordinateFormat(entity))
                     entity.destroy()
@@ -104,6 +128,66 @@ local function on_init()
             end
         end
     end
+end
+
+--shoud return true if entity working(working, normal, low_power, etc)
+local function isStatusIsWorking(machineStatus)
+    for _, status in pairs(validStatuses) do
+        if status == machineStatus then
+            return true
+        end
+    end
+    return false
+end
+
+local function emitters_update(event)
+    if not settings.global["rwse-sync-machine-state-with-emitter"].value then
+        return
+    end
+    for surfaceName, surface in pairs(game.surfaces) do
+        for _, protoName in pairs(global.used_prototypes) do
+            local prew_disabled_entities_positions = global.disabled_entities_positions[surfaceName]
+            local emmiters = surface.find_entities_filtered{type = emitter_type, name = parentName..'__'..protoName}
+            if #emmiters > 0 then
+                for _, emitter in pairs(emmiters) do
+                    local machines = surface.find_entities_filtered{type = proto_type, position = emitter.position}
+                    if #machines == 1 and itsRightEntity(machines[1].name) then
+                        local machine = machines[1]
+                        if not isStatusIsWorking(machine.status) then
+                            debugMsg('detect stopped machine '..coordinateFormat(machine))
+                            table.insert(global.disabled_entities_positions[surfaceName], emitter.position)
+                            emitter.destroy()
+                        end
+                    else
+                        debugMsg('An emitter was found that was not assigned to the machine (was destroyed?), '..coordinateFormat(emitter))
+                        emitter.destroy()
+                    end
+                end
+            end
+
+            for i, position in pairs(prew_disabled_entities_positions) do
+                local machines = surface.find_entities_filtered{type = proto_type, position = position}
+                if #machines == 1 and itsRightEntity(machines[1].name) then
+                    local machine = machines[1]
+                    if isStatusIsWorking(machine.status) then
+                        debugMsg('restore emitter for machine '..coordinateFormat(machine))
+                        local pseudoEvent = {}
+                        pseudoEvent['created_entity'] = machine
+                        on_entity_create(pseudoEvent, true)
+                        prew_disabled_entities_positions[i] = nil
+                    end
+                end
+            end
+        end
+    end
+end
+
+local function on_configuration_changed()
+    debugMsg('detect configuration change')
+    if settings.global["rwse-sync-machine-state-with-emitter"].value then
+        return
+    end
+    on_init()   --In general this is not necessary, but I want to make sure that unserviced machines do not appear
 end
 
 --[[
@@ -129,3 +213,5 @@ script.on_event(defines.events.on_robot_mined_entity, on_entity_delete, filters)
 commands.add_command("rwse-reinit", {"description.command-reinit"}, on_init)
 
 script.on_init(on_init)
+script.on_nth_tick(60, emitters_update)
+script.on_configuration_changed(on_configuration_changed)
